426c5327af0107a6bdec45ca7226edbf91647f82
karanewh <70612794+karanewh@users.noreply.github.com>
2022-04-29 23:22:02 -0400

    line charts

---
+++ viz/hhs-def-line/javascript.js
@@ -0,0 +1,87 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+
+    data: {
+        labels: ["2009-10", "2010-11", "2011-12", "2012-13", "2013-14", "2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2019-20",
+"2020-21", "2021-22"],
+        datasets: [{
+            label: 'Connecticut',
+            data: [65.3, 73.1, 68.3, 70.3, 69.2, 68.9, 68.2, 77.5, 73.7, 80.4, 74.7, 72.9, 79.6],
+            fill: false,
+            borderColor: '#5F3F75',
+            backgroundColor: '#5F3F75',
+            tension: 0.1},
+          {
+            label: 'Baylor',
+            data: [79.0, 76.8, 75.2, 75.9, 83.7, 81.9, 74.9, 75.6, 77.1, 77.8, 70.0, 74.6, 84.1],
+            fill: false,
+            borderColor: '#9D43D9',
+            backgroundColor: '#9D43D9',
+            tension: 0.1},
+        {
+            label: 'Notre Dame',
+            data: [82.1, 76.8, 73.3, 80.2, 82.8, 82.8, 83.9, 87.8, 91.7, 87.2, 91.1, 97.3, 85.9],
+            fill: false,
+            borderColor: '#E3BDFF',
+            backgroundColor: '#E3BDFF',
+            tension: 0.1},
+        {
+            label: 'South Carolina',
+            data: [93.3, 90.8, 79.5, 77.2, 82.0, 79.0, 79.8, 82.3, 87.1, 92.2, 77.2, 80.1, 74.1],
+            fill: false,
+            borderColor: '#E6753C',
+            backgroundColor: '#E6753C',
+            tension: 0.1},
+        {
+            label: 'Stanford',
+            data: [78.4, 79.7, 80.5, 78.9, 85.8, 90.2, 83.4, 82.7, 89.7, 86.6, 86.8, 77.6, 81.6],
+            fill: false,
+            borderColor: '#E6C73C',
+            backgroundColor: '#E6C73C',
+            tension: 0.1},
+        {
+            label: 'Texas A&M',
+            data: [82.5, 80.3, 86.5, 82.4, 83.7, 83.1, 85.9, 93.7, 96.1, 86.3, 84.7, 86.2, 92.7],
+            fill: false,
+            borderColor: '#D99F43',
+            backgroundColor: '#D99F43',
+            tension: 0.1},
+        ]
+    },
+    options: {
+        responsive: true,
+        interaction: {
+            intersect: false,
+            mode: 'index',
+          },
+        plugins: {
+            title: {
+                display: true,
+                text: 'HHS Defensive Ratings',
+                font: {
+                    size: 18
+                }
+            },
+            subtitle: {
+                display: true,
+                text: ["For most of the last 13 years, University of Connecticut was the strongest among any of the six teams that", "won an NCAA championship, according to Her Hoop Stats Ratings. But starting in 2018-19, the gap narrowed,", "and some teams - Baylor, Notre Dame, South Carolina and Stanford - were rated higher than UConn in certain years. The", "HHS Rating is a 'predictive metric of team strength based on machine learning.' It represents the number of points by", "which team would be expected to outscore an average opponent on a neutral court."],
+            }
+        },
+        scales: {
+            x: {
+              title:{
+                display: true,
+                text: 'Season',
+              }
+            },
+            y: {
+                beginAtZero: true,
+                title:{
+                  display: true,
+                  text: 'Her Hoop Stats Rating',
+              }
+            }
+        },
+    },
+});
--- viz/hhs-off-line/javascript.js
+++ viz/hhs-off-line/javascript.js
@@ -7,42 +7,42 @@ var myChart = new Chart(ctx, {
 "2020-21", "2021-22"],
         datasets: [{
             label: 'Connecticut',
-            data: [64.7, 55.6, 57.7, 63.1, 58.9, 67.5, 66.7, 60.3, 62.2, 48.0, 46.7, 45.8, 36.6],
+            data: [114.6, 111.5, 111.0, 116.6, 117.8, 126.4, 123.9, 122.7, 122.1, 116.3, 111.2, 113.0, 105.9],
             fill: false,
             borderColor: '#5F3F75',
             backgroundColor: '#5F3F75',
             tension: 0.1},
           {
             label: 'Baylor',
-            data: [35.0, 52.8, 54.0, 52.9, 39.6, 39.0, 43.9, 54.5, 51.4, 51.4, 48.1, 40.5, 28.6],
+            data: [97.9, 111.8, 112.7, 112.5, 110.9, 109.2, 107.2, 119.0, 117.3, 113.0, 111.1, 109.0, 106.9],
             fill: false,
             borderColor: '#9D43D9',
             backgroundColor: '#9D43D9',
             tension: 0.1},
         {
             label: 'Notre Dame',
-            data: [34.0, 45.9, 52.6, 46.9, 49.2, 47.8, 40.8, 43.8, 44.5, 50.0, 8.3, 6.5, 24.7],
+            data: [101.8, 105.5, 108.4, 110.1, 115.6, 110.7, 109.4, 112.6, 114.2, 117.2, 86.4, 96.2, 100.9],
             fill: false,
             borderColor: '#E3BDFF',
             backgroundColor: '#E3BDFF',
             tension: 0.1},
         {
             label: 'South Carolina',
-            data: [14.0, 11.4, 26.3, 27.6, 35.4, 44.1, 38.2, 44.4, 33.4, 29.4, 49.1, 40.2, 45.0],
+            data: [92.9, 88.4, 94.8, 94.4, 106.6, 110.1, 104.7, 110.3, 106.3, 104.0, 112.0, 104.1, 103.5],
             fill: false,
             borderColor: '#E6753C',
             backgroundColor: '#E6753C',
             tension: 0.1},
         {
             label: 'Stanford',
-            data: [47.0, 49.2, 45.3, 40.9, 37.1, 28.2, 32.6, 36.6, 27.3, 36.4, 33.4, 41.3, 39.3],
+            data: [110.5, 113.9, 110.1, 107.0, 109.7, 104.7, 102.3, 104.2, 97.8, 105.7, 105.1, 111.3, 105.9],
             fill: false,
             borderColor: '#E6C73C',
             backgroundColor: '#E6C73C',
             tension: 0.1},
         {
             label: 'Texas A&M',
-            data: [5.7, 47.4, 28.6, 33.4, 28.5, 21.6, 25.4, 23.9, 29.7, 27.3, 27.4, 26.6, 15.8],
+            data: [102.4, 108.0, 97.1, 99.7,  98.6, 95.9, 98.2, 102.4, 109.4, 99.5, 101.0, 103.0, 95.5],
             fill: false,
             borderColor: '#D99F43',
             backgroundColor: '#D99F43',
@@ -58,14 +58,14 @@ var myChart = new Chart(ctx, {
         plugins: {
             title: {
                 display: true,
-                text: 'Among NCAA Champions, UConn is no longer head and shoulders above the rest',
+                text: 'HHS Offensive Ratings',
                 font: {
                     size: 18
                 }
             },
             subtitle: {
                 display: true,
-                text: ["For most of the last 13 years, University of Connecticut was the strongest among any of the six teams that", "won an NCAA championship, according to Her Hoop Stats Ratings. But starting in 2018-19, the gap narrowed,", "and some teams - Baylor, Notre Dame, South Carolina and Stanford - were rated higher than UConn in certain years. The", "HHS Rating is a 'predictive metric of team strength based on machine learning.' It represents the number of points by", "which team would be expected to outscore an average opponent on a neutral court."],
+                text: ["ABCDEF"],
             }
         },
         scales: {
